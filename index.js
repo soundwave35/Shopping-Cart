f69043969cfc347e6546e65765571b2390fd7dfc
Sıddık Akçay <sdkakcy@gmail.com>
2023-09-08 22:44:41 +0300

    Apply styles

--- index.js
+++ index.js
@@ -1,80 +1,93 @@
-import {initializeApp} from 'firebase/app'
-import {getDatabase, ref, push, onValue, remove, query, equalTo, orderByChild} from "firebase/database"
-import {v4 as uuid} from 'uuid'
+import { initializeApp } from "firebase/app";
+import {
+  getDatabase,
+  ref,
+  push,
+  onValue,
+  remove,
+  query,
+  equalTo,
+  orderByChild
+} from "firebase/database";
+import { v4 as uuid } from "uuid";
 
 const appSettings = {
-    databaseURL: "https://playground-864be-default-rtdb.europe-west1.firebasedatabase.app/"
-}
+  databaseURL:
+    "https://playground-864be-default-rtdb.europe-west1.firebasedatabase.app/"
+};
 
-const app = initializeApp(appSettings)
-const database = getDatabase(app)
-const shoppingListInDB = ref(database, "shoppingList")
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const shoppingListInDB = ref(database, "shoppingList");
 
-const inputFieldEl = document.getElementById("input-field")
-const addButtonEl = document.getElementById("add-button")
-const shoppingListEl = document.getElementById("shopping-list")
+const inputFieldEl = document.getElementById("input-field");
+const addButtonEl = document.getElementById("add-button");
+const shoppingListEl = document.getElementById("shopping-list");
 
-let user = null
+let user = null;
 
-if (localStorage.getItem('user')) {
-    user = localStorage.getItem('user')
+if (localStorage.getItem("user")) {
+  user = localStorage.getItem("user");
 } else {
-    user = uuid()
+  user = uuid();
 
-    localStorage.setItem('user', user)
+  localStorage.setItem("user", user);
 }
 
 addButtonEl.addEventListener("click", function () {
-    let inputValue = inputFieldEl.value
+  let inputValue = inputFieldEl.value;
 
-    if (inputValue === "") {
-        alert("Ürün girmediniz")
+  if (inputValue === "") {
+    alert("Ürün girmediniz");
 
-        return
-    }
+    return;
+  }
 
-    push(shoppingListInDB, {user: user, value: inputValue})
+  push(shoppingListInDB, { user: user, value: inputValue });
 
-    clearInputFieldEl()
-})
+  clearInputFieldEl();
+});
 
-onValue(query(shoppingListInDB, orderByChild('user'), equalTo(user)), function (snapshot) {
+onValue(
+  query(shoppingListInDB, orderByChild("user"), equalTo(user)),
+  function (snapshot) {
     if (snapshot.exists()) {
-        let itemsArray = Object.entries(snapshot.val())
+      let itemsArray = Object.entries(snapshot.val());
 
-        clearShoppingListEl()
+      clearShoppingListEl();
 
-        for (let i = 0; i < itemsArray.length; i++) {
-            let currentItem = itemsArray[i]
+      for (let i = 0; i < itemsArray.length; i++) {
+        let currentItem = itemsArray[i];
 
-            appendItemToShoppingListEl(currentItem)
-        }
+        appendItemToShoppingListEl(currentItem);
+      }
     } else {
-        shoppingListEl.innerHTML = "Hiç ürün girmediniz... :)"
+      shoppingListEl.innerHTML = "Hiç ürün girmediniz... :)";
     }
-})
+  }
+);
 
 function clearShoppingListEl() {
-    shoppingListEl.innerHTML = ""
+  shoppingListEl.innerHTML = "";
 }
 
 function clearInputFieldEl() {
-    inputFieldEl.value = ""
+  inputFieldEl.value = "";
 }
 
 function appendItemToShoppingListEl(item) {
-    let itemID = item[0]
-    let itemValue = item[1]?.value
+  let itemID = item[0];
+  let itemValue = item[1]?.value;
 
-    let newEl = document.createElement("li")
+  let newEl = document.createElement("li");
 
-    newEl.textContent = itemValue
+  newEl.textContent = itemValue;
 
-    newEl.addEventListener("click", function () {
-        let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`)
+  newEl.addEventListener("click", function () {
+    let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`);
 
-        remove(exactLocationOfItemInDB)
-    })
+    remove(exactLocationOfItemInDB);
+  });
 
-    shoppingListEl.append(newEl)
+  shoppingListEl.append(newEl);
 }
